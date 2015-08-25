29a33e134ed0e58d959d1c0fd6fd898f03e1f098
Matt Magoffin <git+matt@msqr.us>
2015-08-25 16:10:16 +1200

    Example custom table cell setup.

--- BRStyleSampler/BRStyleSampler/StyleColorTableViewCell.h
+++ BRStyleSampler/BRStyleSampler/StyleColorTableViewCell.h
@@ -1,16 +1,15 @@
 //
 //  StyleColorTableViewCell.h
-//  MenuSampler
+//  BRStyleSampler
 //
 //  Created by Matt on 23/07/15.
 //  Copyright (c) 2015 Blue Rocket. Distributable under the terms of the Apache License, Version 2.0.
 //
 
-#import <UIKit/UIKit.h>
+#import "SimpleTableViewCell.h"
 
 @class ColorSwatchView;
 
-@interface StyleColorTableViewCell : UITableViewCell
-@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@interface StyleColorTableViewCell : SimpleTableViewCell
 @property (strong, nonatomic) IBOutlet ColorSwatchView *colorSwatch;
 @end
