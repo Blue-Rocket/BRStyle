4c6aa19ad838785625dc15a2f6c238ba3b0e37df
Matt Magoffin <git+matt@msqr.us>
2015-08-27 15:37:15 +1200

    Add more comments.

--- BRStyle/Code/Core/UIBarButtonItem+BRUIStyle.h
+++ BRStyle/Code/Core/UIBarButtonItem+BRUIStyle.h
@@ -10,6 +10,13 @@
 
 #import "BRUIStyle.h"
 
+/**
+ Extension to UIBarButtonItem to allow for stylish objects.
+ 
+ This extension will hook into the @c initWithTitle:style:target:action: method and for any subclass that 
+ conforms to @c BRUIStylishHost will have their style automatically updated, as well as when global style 
+ changes are made.
+ */
 @interface UIBarButtonItem (BRUIStyle)
 
 /** A BRUIStyle object to use. If not configured, the global default style will be returned. */
