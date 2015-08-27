eca670045191ef13eea705753ed985063b5d74e6
Matt Magoffin <git+matt@msqr.us>
2015-08-27 16:08:58 +1200

    Add more comments.

--- BRStyle/Code/Core/BRUIStyleSettings.h
+++ BRStyle/Code/Core/BRUIStyleSettings.h
@@ -154,28 +154,51 @@ NS_ASSUME_NONNULL_BEGIN
 /// @name UI font styles
 ///-------------------------------
 
+/** A font to use for action objects, such as buttons. */
 @property (nonatomic, readwrite) UIFont *actionFont;
+
+/** A font to use for form fields. */
 @property (nonatomic, readwrite) UIFont *formFont;
+
+/** A font to use for navigation items, like navigation bars. */
 @property (nonatomic, readwrite) UIFont *navigationFont;
+
+/** A font to use in alert titles. */
 @property (nonatomic, readwrite) UIFont *alertHeadlineFont;
+
+/** A font to use in alert messages. */
 @property (nonatomic, readwrite) UIFont *alertFont;
 
 ///-------------------------------
 /// @name Structural font styles
 ///-------------------------------
 
+/** A font to use for large, in your face titles. */
 @property (nonatomic, readwrite) UIFont *heroFont;
+
+/** A font to use for major titles. */
 @property (nonatomic, readwrite) UIFont *headlineFont;
+
+/** A font to use for minor titles. */
 @property (nonatomic, readwrite) UIFont *secondaryHeadlineFont;
+
+/** A font to use for normal content text. */
 @property (nonatomic, readwrite) UIFont *textFont;
+
+/** A font to use for captions. */
 @property (nonatomic, readwrite) UIFont *captionFont;
 
 ///-------------------------------
 /// @name List font styles
 ///-------------------------------
 
+/** A font to use in listings, such as table views. */
 @property (nonatomic, readwrite) UIFont *listFont;
+
+/** A font to use for secondary information in listings. */
 @property (nonatomic, readwrite) UIFont *listSecondaryFont;
+
+/** A font to use for captions in listings. */
 @property (nonatomic, readwrite) UIFont *listCaptionFont;
 
 @end
@@ -185,9 +208,16 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface BRMutableUIStyleControlColorSettings : BRUIStyleControlColorSettings
 
+/** An action color to use in controls, often the @c tintColor of a control */
 @property (nonatomic, readwrite) UIColor *actionColor;
+
+/** A border color to use in controls. */
 @property (nonatomic, readwrite) UIColor *borderColor;
+
+/** A gloss effect color to use in controls. */
 @property (nonatomic, readwrite, nullable) UIColor *glossColor;
+
+/** A shadow effect color to use in controls. */
 @property (nonatomic, readwrite, nullable) UIColor *shadowColor;
 
 @end
@@ -197,10 +227,19 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface BRMutableUIStyleControlStateColorSettings : BRUIStyleControlStateColorSettings
 
+/** Color settings to use for a control's normal state. */
 @property (nonatomic, readwrite) BRUIStyleControlColorSettings *normalColorSettings;
+
+/** Color settings to use for a control's highlighted (active) state. */
 @property (nonatomic, readwrite) BRUIStyleControlColorSettings *highlightedColorSettings;
+
+/** Color settings to use for a control's selected state. */
 @property (nonatomic, readwrite) BRUIStyleControlColorSettings *selectedColorSettings;
+
+/** Color settings to use for a control's disabled state. */
 @property (nonatomic, readwrite) BRUIStyleControlColorSettings *disabledColorSettings;
+
+/** Color settings to use for a control's dangerous state. This state represents a destructive action the user should be cautious of executing. */
 @property (nonatomic, readwrite) BRUIStyleControlColorSettings *dangerousColorSettings;
 
 @end
@@ -214,23 +253,39 @@ NS_ASSUME_NONNULL_BEGIN
 /// @name Structural color styles
 ///-------------------------------
 
+/** The primary color to use, often the @c tintColor of the application. */
 @property (nonatomic, readwrite) UIColor *primaryColor;
+
+/** A secondary color to use. */
 @property (nonatomic, readwrite) UIColor *secondaryColor;
+
+/** A background color to use. */
 @property (nonatomic, readwrite) UIColor *backgroundColor;
+
+/** A separator or rule color to use. */
 @property (nonatomic, readwrite) UIColor *separatorColor;
 
 ///-------------------------------
 /// @name Text color styles
 ///-------------------------------
 
+/** The color to apply to hero level text. */
 @property (nonatomic, readwrite) UIColor *heroColor;
+
+/** The color to apply to headline level text. */
 @property (nonatomic, readwrite) UIColor *headlineColor;
+
+/** The color to apply to secondary headline level text. */
 @property (nonatomic, readwrite) UIColor *secondaryHeadlineColor;
+
+/** The color to apply to normal content text. */
 @property (nonatomic, readwrite) UIColor *textColor;
+
+/** The color to apply to caption text. */
 @property (nonatomic, readwrite) UIColor *captionColor;
 
 ///-------------------------------
-/// @name UI font styles
+/// @name UI color styles
 ///-------------------------------
 
 @property (nonatomic, readwrite) UIColor *formColor;
@@ -244,7 +299,10 @@ NS_ASSUME_NONNULL_BEGIN
 /// @name Control color styles
 ///-------------------------------
 
+/** Color settings to apply to controls when shown on top of neutral content, e.g. not toolbars or navigation bars. */
 @property (nonatomic, readwrite) BRUIStyleControlStateColorSettings *controlSettings;
+
+/** Color settings to apply to controls when shown on top of primary colored content, e.g. toolbars or navigation bars. */
 @property (nonatomic, readwrite) BRUIStyleControlStateColorSettings *inverseControlSettings;
 
 @end
