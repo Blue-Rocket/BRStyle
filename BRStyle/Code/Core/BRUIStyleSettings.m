c92ef6d7e44a2bd7bef194a181b6eac8c141206f
Matt Magoffin <git+matt@msqr.us>
2015-09-26 07:14:02 +1200

    Fix inverse dangerous button default colors.

--- BRStyle/Code/Core/BRUIStyleSettings.m
+++ BRStyle/Code/Core/BRUIStyleSettings.m
@@ -584,7 +584,7 @@ static NSString * SettingNameForSelector(BOOL mutable, SEL aSEL, BOOL *setter) {
 	// normal settings
 	inverseControlSettings.normalColorSettings.actionColor = [UIColor whiteColor];
 	inverseControlSettings.normalColorSettings.borderColor = [BRUIStyle colorWithRGBInteger:0x264891];
-	inverseControlSettings.normalColorSettings.glossColor = [inverseControlSettings.normalColorSettings.glossColor colorWithAlphaComponent:0.5];
+	inverseControlSettings.normalColorSettings.glossColor = [inverseControlSettings.normalColorSettings.glossColor colorWithAlphaComponent:0.4];
 	inverseControlSettings.normalColorSettings.shadowColor = [[UIColor whiteColor] colorWithAlphaComponent:0.3];
 	
 	// highlighted settings
@@ -603,8 +603,8 @@ static NSString * SettingNameForSelector(BOOL mutable, SEL aSEL, BOOL *setter) {
 	// dangerous settings
 	BRMutableUIStyleControlColorSettings *dangerousControlColorSettings = [inverseControlSettings.normalColorSettings mutableCopy];
 	dangerousControlColorSettings.fillColor = [BRUIStyle colorWithRGBInteger:0xEB2D38];
-	dangerousControlColorSettings.borderColor = dangerousControlColorSettings.fillColor;
-	defaults[NSStringFromSelector(@selector(dangerousColorSettings))] = (mutable ? dangerousControlColorSettings : [dangerousControlColorSettings copy]);
+	dangerousControlColorSettings.glossColor = nil;
+	inverseControlSettings.dangerousColorSettings = dangerousControlColorSettings;
 	
 	defaults[NSStringFromSelector(@selector(inverseControlSettings))] = (mutable ? inverseControlSettings : [inverseControlSettings copy]);
 
